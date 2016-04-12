8715b2caa3af836cc691e2bd255c75a40491fe45
naosuke <hanakawa@aqte.net>
2016-04-12 21:14:33 +0900

    一定時間ごとに対象の画像が生成される処理を実装

--- shMagApp/GameScene.swift
+++ shMagApp/GameScene.swift
@@ -9,6 +9,9 @@
 import SpriteKit
 
 class GameScene: SKScene {
+
+    var interval:CFTimeInterval!
+
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         self.backgroundColor = SKColor.blackColor()
@@ -36,5 +39,34 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        if interval == nil {
+            interval = currentTime
+        }
+
+        if interval + 1 <= currentTime {
+            createEnemy()
+
+            interval = currentTime
+        }
+    }
+
+    func createEnemy() {
+        let sprite = SKSpriteNode(imageNamed: "Spaceship")
+        sprite.xScale = 0.2
+        sprite.yScale = 0.2
+
+
+        sprite.position.x = CGFloat(randNum())
+        sprite.position.y = CGFloat(randNum())
+
+        print(sprite.position)
+
+        self.addChild(sprite)
+    }
+
+    func randNum() -> UInt! {
+        return UInt(CGRectGetMidX(self.frame))
+            + UInt(arc4random_uniform(UInt32(self.frame.width * 0.9)))
+            - UInt(self.frame.width / 2)
     }
 }
